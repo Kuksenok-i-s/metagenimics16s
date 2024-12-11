15cf1e379a8d78ab56b404eb4c103d776e352415
ikuksenok <kuksyenok.i.s@gmail.com>
2024-12-11 22:12:06 +0000

    Update Dockerfile and main.py for improved configuration and workflow execution
    - Change base image in Dockerfile to amplicon version
    - Simplify dummy_config.yml structure for easier data access
    - Refactor main.py to enhance logging and encapsulate workflow execution in a main function

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-#!/usr/bin/python
+import logging
+from src import Config, DataHandler, ExecutionHandler
+from qiime2 import Artifact, Metadata
 
-from src.config import Config
-from src.data_handler import DataHandler
-config = Config("dummy_config.yml")
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("MainWorkflow")
 
-dh = DataHandler(config)
-dh.run()
+
+def main():
+
+    config = Config("dummy_config.yml").get_config()
+
+    data_handler = DataHandler(config)
+    fmt = data_handler.validate()
+
+    seqs = Artifact.load("emp-single-end-sequences.qza")
+    metadata = Metadata.load("sample-metadata.tsv")
+    classifier = Artifact.load("gg-13-8-99-515-806-nb-classifier.qza")
+
+    computation_handler = ExecutionHandler(seqs, metadata, classifier)
+    results = computation_handler.run_workflow()
+
+    print(results)
+
+if __name__ == "__main__":
+    main()
